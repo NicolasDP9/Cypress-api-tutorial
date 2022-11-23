ada7445ef951da88a42434a71215ae1e442ac867
NicolasDP9 <92314618+NicolasDP9@users.noreply.github.com>
2022-11-23 17:11:42 +0100

    Save - Etape 2

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,12 +11,23 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+const faker = require("faker");
 
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on("task", {
+    freshUser() {
+      user = {
+        nome: faker.name.firstName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        administrador: "true"
+      };
+      return user;
+    }
+  })
+  return config
 }
